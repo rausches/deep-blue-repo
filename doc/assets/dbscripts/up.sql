6ff680c19df6b9d2c2e4d71bc351ded25191c461
Stevie Trapero <sptrapero18@mail.wou.edu>
2025-01-30 19:49:36 -0800

    Added Roles table

--- doc/assets/dbscripts/up.sql
+++ doc/assets/dbscripts/up.sql
@@ -2,11 +2,17 @@ DROP SCHEMA IF EXISTS uxcheckmate;
 CREATE SCHEMA uxcheckmate;
 USE uxcheckmate;
 
+CREATE TABLE Roles (
+    RoleID  INT                 NOT NULL AUTO_INCREMENT,
+    Name    VARCHAR(50)         NOT NULL UNIQUE,
+    PRIMARY KEY (RoleID)
+);
+
 CREATE TABLE UserAccounts (
     UserID      INT             NOT NULL AUTO_INCREMENT,
     Email       VARCHAR(255)    NOT NULL UNIQUE,
     Password    VARCHAR(255)    NOT NULL,
-    RoleID      INT             NOT NULL,
+    RoleID      INT             NOT NULL DEFAULT 0,
     PRIMARY KEY (UserID),
     FOREIGN KEY (RoleID) REFERENCES Roles(RoleID) ON DELETE RESTRICT
 );
