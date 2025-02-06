2c446e1c02f8d21f643e02485cd66b69d1cef3d3
momkage <lexy.b.quijano@gmail.com>
2025-02-06 15:56:36 -0800

    Added link to return home on error page

--- Uxcheckmate/Uxcheckmate_Main/obj/Debug/net9.0/Uxcheckmate_Main.AssemblyInfo.cs
+++ Uxcheckmate/Uxcheckmate_Main/obj/Debug/net9.0/Uxcheckmate_Main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uxcheckmate_Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4006fe2ea32913d40d7c33cc4c3234797cea2af8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa6f7a9df391537e3be9dcc3ece9f14e2ed5e169")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uxcheckmate_Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uxcheckmate_Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
