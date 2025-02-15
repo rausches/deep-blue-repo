2592f7c7896f4530c4ecde1c9bb36685ef1aa1f1
Stevie Trapero <stevetrapero@rocketmail.com>
2025-02-14 18:46:56 -0800

    Added timestamp for isses

--- Uxcheckmate/Uxcheckmate_Main/Models/Pa11yAccessibility.cs
+++ Uxcheckmate/Uxcheckmate_Main/Models/Pa11yAccessibility.cs
@@ -1,13 +1,23 @@
-using System.Collections.Generic;
+using System;
+using TimeZoneConverter;
 
-namespace Uxcheckmate_Main.Models;
-
-public class Pa11yIssue{
+namespace Uxcheckmate_Main.Models
+{
+    public class Pa11yIssue
+    {
         public string Code { get; set; }
         public string Message { get; set; }
         public string Selector { get; set; }
-}
+        public DateTime Timestamp { get; set; } = ConvertToPacificTime(DateTime.UtcNow);
+        private static DateTime ConvertToPacificTime(DateTime utcDateTime)
+        {
+            TimeZoneInfo pacificZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, pacificZone);
+        }
+    }
 
-public class Pa11yResult{
+    public class Pa11yResult
+    {
         public List<Pa11yIssue> Issues { get; set; }
+    }
 }
