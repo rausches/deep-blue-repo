8839574197b0af4efd2c24e3ac2d4dbf7cfd5601
Stevie Trapero <stevetrapero@rocketmail.com>
2025-03-01 10:53:26 -0800

    Added background color test implementation

--- Uxcheckmate/Nunit_Tests/ColorScheme_Tests.cs
+++ Uxcheckmate/Nunit_Tests/ColorScheme_Tests.cs
@@ -20,7 +20,7 @@ namespace Uxcheckmate_Tests
             var color1 = (255, 100, 50);
             var color2 = (205, 100, 50);
             bool result = _colorService.AreColorsSimilar(color1, color2);
-            Assert.IsTrue(result, "Colors should be considered similar.");
+            Assert.That(result, Is.True, "Colors should be considered similar.");
         }
 
         [Test]
@@ -29,14 +29,14 @@ namespace Uxcheckmate_Tests
             var color1 = (255, 255, 255);
             var color2 = (0, 0, 0);
             bool result = _colorService.AreColorsSimilar(color1, color2);
-            Assert.IsFalse(result, "Colors should be considered different.");
+            Assert.That(result, Is.False, "Colors should be considered different.");
         }
 
         [Test]
         public void HexToRgb_ReturnsCorrectRgb()
         {
             var result = ColorSchemeService.HexToRgb("#FF5733");
-            Assert.AreEqual((255, 87, 51), result, "Hex to RGB conversion failed.");
+            Assert.That(result, Is.EqualTo((255, 87, 51)), "Hex to RGB conversion failed.");
         }
 
         [Test]
@@ -61,7 +61,7 @@ namespace Uxcheckmate_Tests
             // Should see that there is blue-text
             var classColors = (Dictionary<string, string>)result["class_colors"];
             Assert.That(classColors.ContainsKey("blue-text"), "Class color should be detected.");
-            Assert.AreEqual("#0000FF", classColors["blue-text"], "Class blue-text should be assigned #0000FF.");
+            Assert.That(classColors["blue-text"], Is.EqualTo("#0000FF"), "Class blue-text should be assigned #0000FF.");
         }
         // Inline-Style Check
         [Test]
@@ -75,7 +75,7 @@ namespace Uxcheckmate_Tests
             var tagColors = (Dictionary<string, string>)result["tag_colors"];
             // Should find color associated with that p
             Assert.That(tagColors.ContainsKey("p"), "Tag color should be detected.");
-            Assert.AreEqual("#FF5733", tagColors["p"], "Paragraph should be assigned #FF5733.");
+            Assert.That(tagColors["p"], Is.EqualTo("#FF5733"), "Paragraph should be assigned #FF5733.");
         }
         // Testing that characters in class are counted towards class and not tag
         [Test]
@@ -92,9 +92,9 @@ namespace Uxcheckmate_Tests
             var classCharacterCount = (Dictionary<string, int>)result["character_count_per_class"];
             var tagCharacterCount = (Dictionary<string, int>)result["character_count_per_tag"];
             // Making sure there are 9 characters in class and none in tag
-            Assert.That(classCharacterCount.ContainsKey("red-text"), "Character count should be assigned to class.");
-            Assert.AreEqual(9, classCharacterCount["red-text"], "Class 'red-text' should have 9 characters.");
-            Assert.False(tagCharacterCount.ContainsKey("h1"), "h1 should not count characters because it's assigned to class.");
+            Assert.That(classCharacterCount, Contains.Key("red-text"), "Character count should be assigned to class.");
+            Assert.That(classCharacterCount["red-text"], Is.EqualTo(9), "Class 'red-text' should have 9 characters.");
+            Assert.That(tagCharacterCount, Does.Not.ContainKey("h1"), "h1 should not count characters because it's assigned to class.");
         }
         // If no color associated with specific tag use then it should default to black
         [Test]
@@ -107,7 +107,7 @@ namespace Uxcheckmate_Tests
             var tagColors = (Dictionary<string, string>)result["tag_colors"];
             // Should have the tag color of h2 associated with h2 to be black
             Assert.That(tagColors.ContainsKey("h2"), "Tag should be detected.");
-            Assert.AreEqual("#000000", tagColors["h2"], "Should default to black.");
+            Assert.That(tagColors["h2"], Is.EqualTo("#000000"), "Should default to black.");
         }
         // No class means character count is added to tag
         [Test]
@@ -120,7 +120,8 @@ namespace Uxcheckmate_Tests
             var tagCharacterCount = (Dictionary<string, int>)result["character_count_per_tag"];
             // p should show up and have 21 characters
             Assert.That(tagCharacterCount.ContainsKey("p"), "Paragraph should be counted.");
-            Assert.AreEqual(21, tagCharacterCount["p"], "Paragraph should have 21 characters.");
+            Assert.That(tagFontSizes["p"], Is.EqualTo(16), "Default font size for <p> should be 16px.");
+
         }
         [Test]
         public void DefaultSizingForTags()
@@ -133,8 +134,8 @@ namespace Uxcheckmate_Tests
             var result = _colorService.ExtractHtmlElements(htmlContent, externalCss);
             var tagFontSizes = (Dictionary<string, int>)result["tag_font_sizes"];
             // h1 needs to be 32 px and p needs to be 16px
-            Assert.AreEqual(32, tagFontSizes["h1"], "Default font size for <h1> should be 32px.");
-            Assert.AreEqual(16, tagFontSizes["p"], "Default font size for <p> should be 16px.");
+            Assert.That(classFontSizes, Contains.Key("large-text"), "Class should have a font size.");
+            Assert.That(classFontSizes["large-text"], Is.EqualTo(24), "Class 'large-text' should have a font size of 24px.");
         }
         [Test]
         public void ShouldUseClassFontSize()
@@ -149,7 +150,7 @@ namespace Uxcheckmate_Tests
             var result = _colorService.ExtractHtmlElements(htmlContent, externalCss);
             var classFontSizes = (Dictionary<string, int>)result["class_font_sizes"];
             Assert.That(classFontSizes.ContainsKey("large-text"), "Class should have a font size.");
-            Assert.AreEqual(24, classFontSizes["large-text"], "Class 'large-text' should have a font size of 24px.");
+            Assert.That(classFontSizes["large-text"], Is.EqualTo(24), "Class 'large-text' should have a font size of 24px.");
         }
         [Test]
         public void TagInlineFontSize()
@@ -161,7 +162,7 @@ namespace Uxcheckmate_Tests
             var tagFontSizes = (Dictionary<string, int>)result["tag_font_sizes"];
             Assert.That(tagFontSizes.ContainsKey("h2"), "Inline font size should be detected.");
             // Should contain inline size
-            Assert.AreEqual(30, tagFontSizes["h2"], "Inline font size for <h2> should override default.");
+            Assert.That(tagFontSizes["h2"], Is.EqualTo(30), "Inline font size for <h2> should override default.");
         }
         [Test]
         public void InlineOverClassFontsize()
@@ -178,10 +179,10 @@ namespace Uxcheckmate_Tests
             var classFontSizes = (Dictionary<string, int>)result["class_font_sizes"];
             // Should still contain class font
             Assert.That(classFontSizes.ContainsKey("medium-text"), "Class font size should be detected.");
-            Assert.AreEqual(22, classFontSizes["medium-text"], "Class 'medium-text' should have 22px.");
+            Assert.That(classFontSizes["medium-text"], Is.EqualTo(22), "Class 'medium-text' should have 22px.");
             // However it should add to inline
             Assert.That(tagFontSizes.ContainsKey("h3"), "Tag font size should be detected.");
-            Assert.AreEqual(28, tagFontSizes["h3"], "Inline font size (28px) should override class size.");
+            Assert.That(tagFontSizes["h3"], Is.EqualTo(28), "Inline font size (28px) should override class size.");
         }
         [Test]
         public void NoFontSizeChangeInClass()
@@ -196,7 +197,7 @@ namespace Uxcheckmate_Tests
             var tagFontSizes = (Dictionary<string, int>)result["tag_font_sizes"];
             // Should set for default
             Assert.That(tagFontSizes.ContainsKey("h4"), "Default font size should be assigned.");
-            Assert.AreEqual(16, tagFontSizes["h4"], "Default size for <h4> should be 16px if not explicitly set.");
+            Assert.That(tagFontSizes["h4"], Is.EqualTo(16), "Default size for <h4> should be 16px if not explicitly set.");
         }
         [Test]
         public void CountCharactersWithFontSizes()
@@ -215,12 +216,42 @@ namespace Uxcheckmate_Tests
             var tagCharacterCount = (Dictionary<string, int>)result["character_count_per_tag"];
             var classCharacterCount = (Dictionary<string, int>)result["character_count_per_class"];
             // Checking each match with size and character amount
-            Assert.AreEqual(30, classFontSizes["big"], "Class 'big' should have font size 30px.");
-            Assert.AreEqual(12, tagFontSizes["p"], "Paragraph should have an inline font size of 12px.");
-            Assert.That(classCharacterCount.ContainsKey("big"), "Class 'big' should have character count.");
-            Assert.AreEqual(4, classCharacterCount["big"], "Class 'big' should count 4 characters from <h1>.");
-            Assert.That(tagCharacterCount.ContainsKey("p"), "Paragraph should count characters.");
-            Assert.AreEqual(5, tagCharacterCount["p"], "Paragraph should count 5 characters.");
+            Assert.That(classFontSizes, Contains.Key("big"), "Class 'big' should have font size.");
+            Assert.That(classFontSizes["big"], Is.EqualTo(30), "Class 'big' should have font size 30px.");
+            Assert.That(tagFontSizes, Contains.Key("p"), "Paragraph should have an inline font size.");
+            Assert.That(tagFontSizes["p"], Is.EqualTo(12), "Paragraph should have an inline font size of 12px.");
+            Assert.That(classCharacterCount, Contains.Key("big"), "Class 'big' should have character count.");
+            Assert.That(classCharacterCount["big"], Is.EqualTo(4), "Class 'big' should count 4 characters from <h1>.");
+            Assert.That(tagCharacterCount, Contains.Key("p"), "Paragraph should count characters.");
+            Assert.That(tagCharacterCount["p"], Is.EqualTo(5), "Paragraph should count 5 characters.");
+        }
+        [Test]
+        public void BackgroundColorShouldBeExtracted()
+        {
+            string htmlContent = @"
+                <style>
+                    body { background-color: #F0F0F0; }
+                </style>
+                <h1>Title</h1>";
+            List<string> externalCss = new List<string>();
+            var result = _colorService.ExtractHtmlElements(htmlContent, externalCss);
+            var backgroundColor = (string)result["background_color"];
+            Assert.That(backgroundColor, Is.EqualTo("#F0F0F0"), "Background color should be extracted.");
+        }
+        [Test]
+        public void BackgroundDefaultsToWhiteIfNoneSpecified()
+        {
+            string htmlContent = @"
+                <html>
+                <head></head>
+                <body>
+                    <h1>Title</h1>
+                </body>
+                </html>";
+            List<string> externalCss = new List<string>();
+            var result = _colorService.ExtractHtmlElements(htmlContent, externalCss);
+            var backgroundColor = (string)result["background_color"];
+            Assert.That(backgroundColor, Is.EqualTo("#FFFFFF"), "Background color should default to white if unspecified.");
         }
     }
 }
