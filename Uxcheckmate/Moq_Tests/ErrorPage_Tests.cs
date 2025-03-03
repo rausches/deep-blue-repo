2e7bc1a766706892f00b4984ba0f241d9efe3eb9
momkage <lexy.b.quijano@gmail.com>
2025-03-03 00:37:39 -0800

    Added AxeCore moq test

--- Uxcheckmate/Moq_Tests/ErrorPage_Tests.cs
+++ Uxcheckmate/Moq_Tests/ErrorPage_Tests.cs
@@ -15,7 +15,7 @@ namespace ErrorPage_Tests
 
         private Mock<UxCheckmateDbContext> _mockDbContext;
 
-        private Mock<IPa11yService> _mockPa11yService;
+        private Mock<IAxeCoreService> _mockAxeCoreService;
 
         private Mock<IReportService> _mockReportService;
         private Mock<HttpClient> _mockHttpClient; 
@@ -27,7 +27,7 @@ namespace ErrorPage_Tests
         {
             _mockLogger = new Mock<ILogger<HomeController>>();
             _mockOpenAiService = new Mock<IOpenAiService>();
-            _mockPa11yService = new Mock<IPa11yService>();
+            _mockAxeCoreService = new Mock<IAxeCoreService>();
             _mockDbContext = new Mock<UxCheckmateDbContext>();
             _mockReportService = new Mock<IReportService>();
             _mockHttpClient = new Mock<HttpClient>();
@@ -39,7 +39,7 @@ namespace ErrorPage_Tests
         public void Error404_ReturnsErrorPageView()
         {
             // Arrange
-           var controller = new HomeController(_mockLogger.Object, _mockHttpClient.Object, _mockDbContext.Object, _mockOpenAiService.Object, _mockPa11yService.Object, _mockReportService.Object, _mockpdfExportService.Object);
+           var controller = new HomeController(_mockLogger.Object, _mockHttpClient.Object, _mockDbContext.Object, _mockOpenAiService.Object, _mockAxeCoreService.Object, _mockReportService.Object, _mockpdfExportService.Object);
 
             // Act
             var result = controller.ErrorPage() as ViewResult;
