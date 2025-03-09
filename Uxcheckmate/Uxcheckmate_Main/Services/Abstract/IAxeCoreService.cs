84490900ba2d5ebddd37595d1718a911ffbb79a4
ajacho <belen.jacho18@gmail.com>
2025-03-08 23:49:02 -0800

    Added services for playwright and screenshot

--- Uxcheckmate/Uxcheckmate_Main/Services/Abstract/IAxeCoreService.cs
+++ Uxcheckmate/Uxcheckmate_Main/Services/Abstract/IAxeCoreService.cs
@@ -14,6 +14,8 @@ namespace Uxcheckmate_Main.Services
 {
   public interface IAxeCoreService
   {
+    // This method is responsible for performing an accessibility analysisn on a given URL.
+    // Using Axe-core withing a headless browser powered by Playwright and saving the results to the database.
     Task<ICollection<AccessibilityIssue>> AnalyzeAndSaveAccessibilityReport(Report report);
   }
 }
