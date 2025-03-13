1bb23011649d09b872172132eace9383dd82c657
momkage <lexy.b.quijano@gmail.com>
2025-03-12 19:12:38 -0700

    Added nunit test for Filtering

---
+++ Uxcheckmate/Moq_Tests/Controller_Tests/FilteringController_Tests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+using Uxcheckmate_Main.Services;
+
+namespace Controller_Tests
+{
+    public class FilterController_Tests
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+            // filteringcontroller
+            // mockdb
+            // logger
+        }
+
+        // private class to mock data
+
+        // WhenIclickalphabetical_itemsaredisplayedalphabetically
+            // runfilteringmethod on mocked data
+            // assert that items are alpabetical
+        // WhenIclickpriorityhigh_itemsaredisplaybypriorityfromhightolow
+        // WhenIclickprioritylow_itemsaredisplayedbyproprityfromlowtohigh
+
+    }
+}
