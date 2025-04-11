b55afa7d2b5203663d63d435e0446b892387745b
momkage <lexy.b.quijano@gmail.com>
2025-04-11 11:52:41 -0700

    ScrapeAsync method added using Playwright

---
+++ Uxcheckmate/Uxcheckmate_Main/Services/Abstract/IPopUpsService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Playwright;
+
+namespace Uxcheckmate_Main.Services
+{
+    public interface IPopUpsService
+    {
+        
+    }
+}
