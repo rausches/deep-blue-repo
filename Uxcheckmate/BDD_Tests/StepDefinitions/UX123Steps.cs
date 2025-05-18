85942f1aa5ed57d19a41f846adfd1130fcbc9dea
momkage <lexy.b.quijano@gmail.com>
2025-05-18 12:50:59 -0700

    Created ux123 bdd file

---
+++ Uxcheckmate/BDD_Tests/StepDefinitions/UX123Steps.cs
@@ -0,0 +1,112 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Reqnroll;
+using Microsoft.Extensions.Logging;
+using Uxcheckmate_Main.Services;
+
+namespace BDD_Tests.StepDefinitions
+{
+    [Binding]
+    public class UX123Steps
+    {
+        private readonly IWebDriver _driver;
+        private readonly ScenarioContext _scenarioContext;
+
+        public UX123Steps(IWebDriver driver, ScenarioContext scenarioContext){
+            _driver = driver;
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the design issues are still loading")]
+        public void GivenTheDesignIssuesAreStillLoading()
+        {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(300));
+
+            // Wait for design issues overlay to be visible
+            var loadingOverlay = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("designIssuesOverlay")));
+
+            // Assert that overlay is visible
+            Assert.That(loadingOverlay.Displayed, Is.True, "Overlay should be visible");
+        }
+
+        [Then("they should be able to open the accessibility issues")]
+        public void ThenTheyShouldBeAbleToViewTheAccessibilityIssues()
+        {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(300));
+
+            // Find accessibility Issues
+            var accessibilityIssues = wait.Until(driver => driver.FindElement(By.Id("Modal")));
+
+            // Click accessibility issues
+            accessibilityIssues.Click();
+
+            // Find accessibility text
+            var accessibilityText = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("designIssuesOverlay")));
+
+            // Assert that accessibility text is display
+            Assert.That(AccessibilityText.Displayed, Is.True, "Overlay should be visible");
+        }
+
+        [Then("they will not be able to sort")]
+        public void ThenTheyWillNotBeAbleToSort()
+        {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(300));
+
+            // Find sort element
+            var sortSelect = wait.Until(driver => driver.FindElement(By.Id("sortSelect")));
+
+            // Assert that select is inactive
+            Assert.That(sortSelect);
+
+        }
+
+        [Given("they sort by severity")]
+        public void GivenTheySortBySeverity()
+        {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(300));
+
+            // Find sort element
+            var sortSelect = wait.Until(driver => driver.FindElement(By.Id("sortSelect")));
+
+            // Click sort element to open
+            sortSelect.Click();
+
+            // Find sort method 
+            var severitySort = wait.Until(driver => driver.FindElement(By.Id("sortSelect")));
+
+            // Click that method
+            severitySort.Click();
+        }
+
+        [Then("they will be able to sort")]
+        public void ThenTheyWillBeAbleToSort()
+        {
+            // wait
+            // find sort element
+            // assert that it is active
+        }
+
+        [Then("they should not see the modal pop up again")]
+        public void GivenTheyShouldNotSeeTheModalPopUpAgain()
+        {
+            // wait
+            // find modal
+            // assert that modal is not visible
+        }
+
+        [Given("they click the summary button")]
+        public void GivenTheyClickTheSummaryButton()
+        {
+            // wait 
+            // find button
+            // click up button
+            // find modal
+            // assert that modal is visible
+        }
+    }
+}
--- Uxcheckmate/BDD_Tests/features/UX123.feature
+++ Uxcheckmate/BDD_Tests/features/UX123.feature
@@ -4,14 +4,16 @@ Scenario: Accessibility issues are clickable while rest of the report loads
     Given the user generates a report
     When they get to the results page
     And the design issues are still loading 
-    And the accessibility issues are done loading
     Then they should be able to view the accessibility issues
 
 Scenario: Sort is not available until report is done loading
     Given the user generates a report
     And they get to the results page
-    And the report is still loading
+    And the design issues are still loading
     Then they will not be able to sort
+    And the report is done loading 
+    And the user clicks the let's begin button
+    Then they will be able to sort
 
 Scenario: Modal pops up once then is view via button
     Given the user generates a report
@@ -19,5 +21,5 @@ Scenario: Modal pops up once then is view via button
     And they click out of the summary modal
     And they sort by severity
     Then they should not see the modal pop up again
-    And they click the summary button
-    Then the modal should pop up again.
+    And the user clicks the let's begin button
+    Then the user will see a modal containing the summary
