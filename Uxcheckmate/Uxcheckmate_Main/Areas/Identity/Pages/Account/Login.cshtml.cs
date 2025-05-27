ee29110b9200351d363129f91b57869bb39c752e
Stevie Trapero <stevetrapero@rocketmail.com>
2025-05-26 19:45:42 -0700

    Added the captcha to login and register

--- Uxcheckmate/Uxcheckmate_Main/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Uxcheckmate/Uxcheckmate_Main/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -21,12 +21,19 @@ namespace TempIdentityProject.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly ICaptchaService _captchaService;
+        private readonly IConfiguration _configuration;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger, ICaptchaService captchaService, IConfiguration configuration)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _captchaService = captchaService;
+            _configuration = configuration;
         }
+
+        [BindProperty]
+        public string CaptchaToken { get; set; }
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -90,6 +97,7 @@ namespace TempIdentityProject.Areas.Identity.Pages.Account
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
+            ViewData["CaptchaSiteKey"] = _configuration["Captcha:SiteKey"];
 
             returnUrl ??= Url.Content("~/");
 
@@ -105,13 +113,21 @@ namespace TempIdentityProject.Areas.Identity.Pages.Account
         {
             returnUrl ??= Url.Content("~/");
 
+            ViewData["CaptchaSiteKey"] = _configuration["Captcha:SiteKey"];
+
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            if (string.IsNullOrWhiteSpace(CaptchaToken) || !await _captchaService.VerifyTokenAsync(CaptchaToken)){
+                ModelState.AddModelError("CaptchaToken", "Please complete the CAPTCHA to continue.");
+                return Page();
+            }
 
             if (ModelState.IsValid)
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                ViewData["CaptchaSiteKey"] = _configuration["Captcha:SiteKey"];
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
@@ -132,6 +148,7 @@ namespace TempIdentityProject.Areas.Identity.Pages.Account
                     return Page();
                 }
             }
+
 
             // If we got this far, something failed, redisplay form
             return Page();
